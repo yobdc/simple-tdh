b35edb372d6b2234ec6d9053dce5a1f5bc6115a8
Lex <yobdc.101@gmail.com>
2018-08-09 15:25:23 +0800

    字段搜索

--- src/main/java/hello/controller/ElasticSearchController.java
+++ src/main/java/hello/controller/ElasticSearchController.java
@@ -1,7 +1,10 @@
 package hello.controller;
 
 import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +21,15 @@ public class ElasticSearchController {
                 .setOperationThreaded(false)
                 .get();
         return response;
+    }
+
+    @RequestMapping("/es/search")
+    public String search(String index, String type, String field, String value) {
+        SearchResponse response = esClient.prepareSearch(index)
+                .setTypes(type)
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setQuery(QueryBuilders.matchQuery(field, value))
+                .get();
+        return response.toString();
     }
 }
